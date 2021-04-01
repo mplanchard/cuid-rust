0ae53abcc373e825abf6b0183d6f7b117276b96d
Matthew Planchard <msplanchard@gmail.com>
2021-04-01 14:06:12 -0500

    test: speed up collision tests, use fewer slugs
    slugs are not as unique as CUIDs, so test with fewer of them

--- tests/collisions.rs
+++ tests/collisions.rs
@@ -6,45 +6,44 @@ use std::{collections::HashSet, thread};
 #[test]
 fn check_cuid_collisions() {
     let mut set = HashSet::new();
-    for _ in 0..1200000 {
+    for _ in 0..1_200_000 {
         let id = cuid::cuid().unwrap();
-        assert!(!set.contains(&id));
         set.insert(id);
     }
+    // we generated 1.2 million unique CUIDs
+    assert!(set.len() == 1_200_000)
 }
 
 #[test]
 /// Build 1.2e6 slugs
 fn check_slug_collisions() {
     let mut set = HashSet::new();
-    for _ in 0..1200000 {
+    for _ in 0..600_000 {
         let id = cuid::slug().unwrap();
-        assert!(!set.contains(&id));
         set.insert(id);
     }
+    // we generated 600k unique slugs
+    assert!(set.len() == 600_000)
 }
 
 #[test]
 /// Ensure we don't get any collisions in 10 threads, each generating 1.2e6 CUIDs
 fn check_cross_thread_collisions() {
-    let sets: Arc<Mutex<Vec<Arc<Mutex<HashSet<_>>>>>> = Arc::new(Mutex::new(vec![]));
+    let sets: Arc<Mutex<Vec<HashSet<_>>>> = Arc::new(Mutex::new(vec![]));
     let mut threads: Vec<thread::JoinHandle<_>> = vec![];
+    let ids_per_thread = 1_200_000;
+    let num_threads = 10;
 
-    let mut local_sets = sets.lock().unwrap();
-    for _ in 0..10 {
-        local_sets.push(Arc::new(Mutex::new(HashSet::new())));
-    }
-    drop(local_sets);
-
-    for i in 0..10 {
+    for _ in 0..num_threads {
         let thread_sets = sets.clone();
         let t = thread::spawn(move || {
-            for _ in 0..1200000 {
-                let sets = thread_sets.lock().unwrap();
-                let mut set = sets[i].lock().unwrap();
+            let mut thread_set = HashSet::new();
+            for _ in 0..ids_per_thread {
                 let id = cuid::cuid().unwrap();
-                set.insert(id);
+                thread_set.insert(id);
             }
+            let mut sets = thread_sets.lock().unwrap();
+            sets.push(thread_set);
         });
         threads.push(t);
     }
@@ -54,8 +53,9 @@ fn check_cross_thread_collisions() {
     });
 
     let local_sets = sets.lock().unwrap();
-    let intersection: HashSet<_> = (*local_sets).iter().fold(HashSet::new(), |acc, i| {
-        let set = i.lock().unwrap();
+    assert!(local_sets.len() == num_threads);
+    let intersection: HashSet<_> = (*local_sets).iter().fold(HashSet::new(), |acc, set| {
+        assert!(set.len() == ids_per_thread);
         acc.intersection(&set)
             .into_iter()
             .map(|i| i.to_owned())
