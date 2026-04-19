ca19d9a62c9fedd76f750e77790dfeaf9a0f10e4
Matthew Planchard <msplanchard@gmail.com>
2026-04-18 23:17:49 -0400

    remove unused variable

--- crates/cuid2/src/lib.rs
+++ crates/cuid2/src/lib.rs
@@ -79,10 +79,6 @@ pub const DEFAULT_LENGTH: u8 = 24;
 const BIG_LENGTH: u8 = 32;
 // valid characters to start an ID
 const STARTING_CHARS: &[u8] = b"abcdefghijklmnopqrstuvwxyz";
-const B36_LOOKUP: [char; 36] = [
-    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
-    'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-];
 
 // =============================================================================
 // THREAD LOCALS
